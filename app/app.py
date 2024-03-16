477348e6c917e86975bc771d83b2d4f1e0d48a8f
Piotr <piotr.wolinski.2017@outlook.com>
2024-03-16 14:23:00 +0100

    Add sonar cloud setup

--- app/app.py
+++ app/app.py
@@ -3,8 +3,19 @@ import json
 
 from models import InventoryRequest
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 INVENTORY_BASE_URL = "http://inventory-service"
 LLM_BASE_URL = "http://llm-service:8888"
