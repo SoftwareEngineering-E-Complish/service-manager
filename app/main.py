f9a2b75e6c5a11158ba56831adf8ce625c42594d
Piotr <piotr.wolinski.2017@outlook.com>
2024-04-02 16:31:29 +0200

    Establish proxy between services

--- app/main.py
+++ app/main.py
@@ -1,29 +1,30 @@
 import logging
+from functools import partial
+from pathlib import Path
 
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from app.models import InventoryRequest
-from app.utils import fetch_json, get_data_from_llm
 
 from app.config import (
-    LLM_QUERY_ENDPOINT_TEMPLATE,
-    PROPERTY_QUERY_ENDPOINT_TEMPLATE,
-    QUERY_SCHEMA_ENDPOINT_TEMPLATE,
+    INVENTORY_SERVICE_URL,
+    LLM_QUERY_ENDPOINT,
+    PROPERTY_QUERY_ENDPOINT,
+    QUERY_SCHEMA_ENDPOINT,
+    USER_SERVICE_URL,
 )
+from app.models import InventoryRequest
+from app.utils import _reverse_auth_proxy, _reverse_proxy, fetch_json, get_data_from_llm
 
-QUERY_SCHEMA_ENDPOINT = QUERY_SCHEMA_ENDPOINT_TEMPLATE.format(
-    "inventory-service"
-)
-LLM_QUERY_ENDPOINT = LLM_QUERY_ENDPOINT_TEMPLATE.format("llm-service:8888")
-PROPERTY_QUERY_ENDPOINT = PROPERTY_QUERY_ENDPOINT_TEMPLATE.format(
-    "inventory-service"
-)
+LOG_CONFIG_PATH = Path(__file__).parent / "log_config.yaml"
+
 INTERNAL_SERVER_ERROR = 500
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 
-logger = logging.getLogger(__file__)
 
 origins = ["*"]
 
@@ -35,10 +36,52 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Inventory service
+app.add_route(
+    "/properties",
+    partial(_reverse_proxy, INVENTORY_SERVICE_URL),
+    methods=["GET", "POST"],
+)
+app.add_route(
+    "/properties/{path:path}",
+    partial(_reverse_proxy, INVENTORY_SERVICE_URL),
+    methods=["GET"],
+)
+app.add_route(
+    "/queryProperties", partial(_reverse_proxy, INVENTORY_SERVICE_URL), methods=["GET"]
+)
 
-@app.get("/properties/")
+# User service without authorization
+app.add_route("/loginURL", partial(_reverse_proxy, USER_SERVICE_URL), methods=["GET"])
+app.add_route("/signupURL", partial(_reverse_proxy, USER_SERVICE_URL), methods=["GET"])
+app.add_route("/logoutURL", partial(_reverse_proxy, USER_SERVICE_URL), methods=["GET"])
+app.add_route("/session", partial(_reverse_proxy, USER_SERVICE_URL), methods=["GET"])
+app.add_route(
+    "/verifyAccessToken", partial(_reverse_proxy, USER_SERVICE_URL), methods=["GET"]
+)
+app.add_route(
+    "/refreshAccessToken", partial(_reverse_proxy, USER_SERVICE_URL), methods=["GET"]
+)
+
+# User service with authorization
+app.add_route(
+    "/userId", partial(_reverse_auth_proxy, USER_SERVICE_URL), methods=["GET"]
+)
+app.add_route("/user", partial(_reverse_auth_proxy, USER_SERVICE_URL), methods=["GET"])
+app.add_route(
+    "/updateUser", partial(_reverse_auth_proxy, USER_SERVICE_URL), methods=["POST"]
+)
+app.add_route(
+    "/deleteUser", partial(_reverse_auth_proxy, USER_SERVICE_URL), methods=["GET"]
+)
+app.add_route(
+    "/changePassword", partial(_reverse_auth_proxy, USER_SERVICE_URL), methods=["POST"]
+)
+
+
+@app.get("/initial_query")
 async def list_properties(user_query: str):
-    logging.info(f"Received user query = {user_query}")
+    logger.info(f"Received user query = {user_query}")
 
     query_schema = fetch_json(QUERY_SCHEMA_ENDPOINT)
 
@@ -52,7 +95,7 @@ async def list_properties(user_query: str):
         query=user_query, api_documentation=query_schema
     ).model_dump()
 
-    logging.info(f"Fetched query schema from inventory = {data}")
+    logger.info(f"Fetched query schema from inventory = {data}")
 
     res_status_code, llm_query = get_data_from_llm(LLM_QUERY_ENDPOINT, data)
 
@@ -72,10 +115,15 @@ async def list_properties(user_query: str):
             detail="Something went wrong with the inventory service. Querying properties failed.",
         )
 
-    logging.info(f"Fetched data from inventory = {inventory_res}")
+    logger.info(f"Fetched data from inventory = {inventory_res}")
 
     return inventory_res
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_config=str(LOG_CONFIG_PATH),
+    )
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,13 @@
+import logging
 import httpx
+from config import TOKEN_VERIFICATION_ENDPOINT
+from fastapi import HTTPException, Request
+from fastapi.responses import Response
+
 from app.models import LLMQuery
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def fetch_json(endpoint: str, params: dict | None = None) -> dict | None:
@@ -23,3 +31,45 @@ def get_data_from_llm(endpoint: str, data: dict) -> tuple[int, LLMQuery | None]:
         llm_query = LLMQuery(**res.json())
 
     return res.status_code, llm_query
+
+
+async def _reverse_proxy(call_url: str, request: Request):
+    query = request.url.query.encode("utf-8")
+    
+    url = httpx.URL(
+        path=request.url.path,
+        query=query if query else None,
+    )
+
+    async with httpx.AsyncClient(base_url=f"http://{call_url}") as client:
+        rp_req = client.build_request(request.method, url, headers=request.headers.raw)
+        logger.info(f'Redirect URL = {rp_req}')
+        rp_resp = await client.send(rp_req)
+
+        return Response(
+            rp_resp.content,
+            status_code=rp_resp.status_code,
+            headers=rp_resp.headers,
+        )
+
+
+async def _reverse_auth_proxy(call_url: str, request: Request):
+    token = request.headers.get("Authorization")
+
+    if token is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Request does not contain authorization token.",
+        )
+
+    auth_response = httpx.get(
+        TOKEN_VERIFICATION_ENDPOINT, params={"accessToken": token}
+    )
+
+    if auth_response.text == "false":
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authorization token.",
+        )
+
+    return await _reverse_proxy(call_url, request)
